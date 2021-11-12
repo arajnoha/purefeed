550babf418086116b5addb5a98cd78b6215f55f9
Adam Rajnoha <adam.rajnoha@webnode.com>
2021-11-12 16:23:51 +0100

    view counts

--- index.php
+++ index.php
@@ -15,6 +15,17 @@ if (isset($_GET["search-string"])) {
 }
 
 
+if (!isset($_SESSION["visit"])) {
+    $_SESSION["visit"] = 1;
+    $viewFile = [];
+    if (file_exists("view.json")) {
+        $viewFile = json_decode(file_get_contents("view.json"), true);
+    }
+    $viewDate = date('Y-m-d');
+    $viewFile[$viewDate] += 1;
+    file_put_contents("view.json", json_encode($viewFile));
+}
+
 ?>
 <!doctype html>
 <html lang="cs">
